06bac47252a8379b7536366bc4d2ef8acf519231
Michael Mayfield <mam0279@auburn.edu>
2023-02-09 16:52:28 -0600

    Class on 2/9/23

---
+++ matrixes and stuff.R
@@ -0,0 +1,62 @@
+# This is a comment - annotate code with #
+#ctrl+enter is shortcut to run
+4-4
+x = 69
+y <- 69
+x+y
+x*y*y*y*
+animal <- "monkey"
+name <- "Bobo Peterson"
+class(seven)
+x = 4
+class(x)
+vec <- c(5,6,7,8,9) # numeric vector
+vec <- (1:17)
+vec
+vec2 <- ("Monkey", "Bobo Peterson")
+vec2
+vec3 <- c(TRUE, FALSE)
+vec3
+vec2 [2]
+vec2[2]
+vec2[2]
+vec + x
+mean(vec)
+sd(vec)
+sd(veC)/sqrt(7)
+sd(vec)/sqrt(7)
+sum(vec)
+median(vec)
+summary(vec
+summary(vec)        
+summary(vec)
+exp(vec)
+> # greater than
+  <
+  | #or
+  & #and
+  >=
+  != #not equal to
+t <- 1:10
+t[(t > 8)]|[(t < 5)]
+t[t != 2]
+1 %in% t
+mat1 <- matrix(data = c(1,2,3), nrow = 3, ncol = 3)
+mat1
+mat2 <- matrix(data = c("Tom","Dick","Harry"), nrow = 3, ncol = 3)
+mat2
+mat1[5]
+mat1[2,2]
+mat1[,2]
+mat2[,2]
+mat2[2,]
+df <- data.frame(mat1[,1], mat2[2,])
+df
+colnames(df) <- c("Monkeys", "Monkey names")
+dfdf[,2]
+df[,2]
+df$`Monkey names`
+
+df$Monkeys[df$`Monkey names` == "Dick"]
+
+subset(df, name == "Dick")
