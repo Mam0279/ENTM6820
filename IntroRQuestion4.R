8aad895007c04338384beb2ed830c8d7bf09859b
Michael Mayfield <mam0279@auburn.edu>
2023-02-28 11:41:39 -0600

    Question 4 from the assignment Intro to R

---
+++ IntroRQuestion4.R
@@ -0,0 +1,11 @@
+vec <- c(1:200)
+mean (vec)
+sd (vec)
+vec c (1 > True, 1 = False, 1 < False,)
+df <- data.frame(mat1[,Z], mat2[vec c,])
+colnames(df) <- c("Z", "Zlog")
+col(sqrt:"Z")
+  colnames("zsquared")
+df
+"zsquared"[(zsquared > 10) | (zsquared < 100)]
+###I could not get this code to run but I hope this attempt is close to working
